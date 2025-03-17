44fd19843002aa91207332c206f085e933130b71
javier.grima@gracia.lasalle.cat <javier.grima@gracia.lasalle.cat>
2025-03-17 09:06:10 +0100

    Añadir constructor Cotxe

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -2,4 +2,10 @@ package org.JavaCar;
 
 public class Cotxe extends Vehicle {
     private int nombrePlaces;
+
+    public Cotxe(String matricula, String marca, String modelo, int preuBase, String motor,
+                 int rodes, boolean etiquetaAmbiental, int nombrePlaces) {
+        super(matricula, marca, modelo, preuBase, motor, rodes, etiquetaAmbiental);
+        this.nombrePlaces = nombrePlaces;
+    }
 }
