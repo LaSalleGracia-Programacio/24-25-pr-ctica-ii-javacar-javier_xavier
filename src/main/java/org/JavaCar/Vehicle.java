81a1594b1f2a6abc435173296c6a1bc3f2f827b6
javier.grima@gracia.lasalle.cat <javier.grima@gracia.lasalle.cat>
2025-03-21 13:38:54 +0100

    Corrección clase Vehicle

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -1,11 +1,46 @@
 package org.JavaCar;
 
-public class Vehicle {
-    private String Matricula;
-    private String Marca;
-    private String Modelo;
+abstract class Vehicle implements LlogableGestor {
+    protected String matricula;
+    protected String marca;
+    protected String model;
     private int preuBase;
-    private String motor;
-    private int Rodes;
+    private Motor motor;
+    private Roda[] rodes;
     private boolean etiquetaAmbiental;
+
+    public Vehicle(String matricula, String marca, String model, int preuBase, Motor motor, Roda[] rodes, boolean etiquetaAmbiental) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.model = model;
+        this.preuBase = preuBase;
+        this.motor = motor;
+        this.rodes = rodes;
+        this.etiquetaAmbiental = etiquetaAmbiental;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public String getModel() {
+        return model;
+    }
+    public int getPreuBase() {
+        return preuBase;
+    }
+    public Motor getMotor() {
+        return motor;
+    }
+    public Roda[] getRodes() {
+        return rodes;
+    }
+
+    @Override
+    public double calcularPreu(int dies) {
+        return preuBase * dies;
+    }
 }
+
