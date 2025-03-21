54cc68980273fe5b89fa77fc990ea6744fb1a8cc
javier.grima@gracia.lasalle.cat <javier.grima@gracia.lasalle.cat>
2025-03-21 12:40:19 +0100

    corregir clase Moto

--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -2,4 +2,26 @@ package org.JavaCar;
 
 public class Moto extends Vehicle{
     private int Cilindradas;
+class Moto extends Vehicle {
+    private final int cilindrada;
+
+    public Moto(String matricula, String marca, String model, int preuBase,
+                int cilindrada, Motor motor, Roda[] rodes) {
+        super(matricula, marca, model, preuBase, motor, rodes, false);
+        this.cilindrada = cilindrada;
+    }
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
+
+    @Override
+    public double calcularPreu(int dies) {
+        double preuBase = getPreuBase() * dies;
+        if (cilindrada > 500) {
+            return preuBase + (5 * dies);
+        }
+        return preuBase;
+    }
 }
+
