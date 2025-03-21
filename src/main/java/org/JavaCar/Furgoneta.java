8360c3bdfb4f17f7eed543b33d85115be893055d
javier.grima@gracia.lasalle.cat <javier.grima@gracia.lasalle.cat>
2025-03-21 12:08:50 +0100

    Corrección clase furgoneta

--- src/main/java/org/JavaCar/Furgoneta.java
+++ src/main/java/org/JavaCar/Furgoneta.java
@@ -4,8 +4,22 @@ class Furgoneta extends Vehicle {
     private double capacitatCarga;
 
     public Furgoneta(String matricula, String marca, String model, int preuBase,
-                     double capacitatCarga, String motor, int rodes, boolean etiquetaAmbiental) {
-        super(matricula, marca, model, preuBase, motor, rodes, etiquetaAmbiental);
+                     double capacitatCarga, String motor, Integer rodes) {
+        super(matricula, marca, model, preuBase, motor, rodes, false);
         this.capacitatCarga = capacitatCarga;
     }
+
+    public double getCapacitatCarga() {
+        return capacitatCarga;
+    }
+
+    public double calcularPreu(int dies) {
+        double preuBase = getPreuBase() * dies;
+        if (capacitatCarga > 1000) {
+            return preuBase + (10 * dies);
+        }
+        return preuBase;
+    }
 }
+
+
