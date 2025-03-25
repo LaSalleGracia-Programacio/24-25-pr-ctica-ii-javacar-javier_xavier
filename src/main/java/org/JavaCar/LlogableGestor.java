460997a551aed17feefc4ef2111a19ddfc3efbc6
javier.grima@gracia.lasalle.cat <javier.grima@gracia.lasalle.cat>
2025-03-25 20:49:28 +0100

    correccion clase LogableGestor

--- src/main/java/org/JavaCar/LlogableGestor.java
+++ src/main/java/org/JavaCar/LlogableGestor.java
@@ -1,6 +1,20 @@
 package org.JavaCar;
 
-public interface LlogableGestor {
+import java.util.List;
+
+interface LlogableGestor {
     double calcularPreu(int dies);
+}
+
+class GestorLloguers {
+    public static double calcularIngressosTotals(List<Vehicle> vehicles, int dies) {
+        double total = 0;
+        for (Vehicle vehicle : vehicles) {
+            if (vehicle instanceof LlogableGestor) {
+                total += ((LlogableGestor) vehicle).calcularPreu(dies);
+            }
+        }
+        return total;
+    }
 }
 
